6de33b49e0f9462b16a449843813b74523cc7c4f
Rediat <rediatbrook@gmail.com>
2024-11-17 19:45:22 -0600

    Creating student and team accounts with create teams

--- api/usernames.py
+++ api/usernames.py
@@ -1,6 +1,6 @@
 from passwords import generate_password
 
-def generate_username(first_name: str, last_name: str):
+def generate_username(first_name: str, last_name: str = ""):
     full_name: str = first_name+last_name
     random_add_on: str = generate_password(7)
     username: str = f"{full_name}_{random_add_on}"
